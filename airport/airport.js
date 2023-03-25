daa23eae223568df5a3bcc9a6f7c7f261b7e9b77
“Saki <sakisandra.c@gmail.com>
2023-03-25 10:14:16 -0600

    Create passing functions

--- airport/airport.js
+++ airport/airport.js
@@ -1,7 +1,50 @@
+function createAirport(name, airlines, gates){
+  let airport ={
+    name: name,
+    airlines: airlines,
+    availableGates: gates,
+  }
+  return airport;
+}
+function welcomeGuests(airport){
+  return `Welcome to ${airport.name}!`
+}
 
+function landPlanes(airport, gates){
+  airport.availableGates = airport.availableGates - gates
+    if(airport.availableGates > 0){
+      return `Success! Current availability is ${airport.availableGates}.`
+    } else { 
+      let overflow = -airport.availableGates
+      airport.availableGates = 0;
+      return `Oh no! Not enough gates available. Current overflow is ${overflow}.`
+    }
+  }
+
+// function checkAirlineLocations(airports, airline){
+//   let locations = [];
+//   for (let i=0; i < airports.length; i++){
+//     if (airports[i].airlines.includes(airline)){
+//       locations.push(airports[i].name)
+//     }
+//   } return locations
+// }
+
+function checkAirlineLocations(airports, airline){
+  let locations = airports
+  .filter((airport) => {
+    if(airport.airlines.includes(airline)){
+    return airport
+    }
+  })
+  .map((airport)=>{
+    return airport.name
+  })
+  return locations
+}
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlane, 
-  // checkAirlineLocations
+  createAirport, 
+  welcomeGuests, 
+  landPlanes,
+  checkAirlineLocations
 };
