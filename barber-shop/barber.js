dd77185105e9ab4286edcb2517fc70634ca8f2d2
“Saki <sakisandra.c@gmail.com>
2023-03-25 10:40:01 -0600

    Create barber passing functions

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,41 @@
+function createBarber(name, earnings =0 , cuts= []){
+    let barber = {
+        name: name,
+        earnings: earnings,
+        haircuts: cuts,
+        knownCuts: cuts,
+    }
+    return barber;
+}
 
+function giveCompliment(cut){
+   return `This ${cut.style} looks great!`
+}
 
-module.exports = {  };
+function cutHair(person, cut){
+    person.haircuts.push(cut)
+    person.earnings = person.earnings + cut.price
+    return person
+}
+
+function listStyles(person, style){
+    let styles = [];
+    for (i=0; i < person.knownCuts.length; i++){
+        if(person.knownCuts[i].hairLength === style)
+       styles.push(person.knownCuts[i].style);
+    }
+    return styles;
+} 
+
+module.exports = {  
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles,
+};
+
+// mattCanDoMohawk = {
+//     name: 'Matt',
+//     earnings: 0,  
+//     haircuts: [{ style: 'mohawk', hairLength: 'short', price: 11.00 }]
+// }
