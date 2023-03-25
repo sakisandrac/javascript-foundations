c77afd7c468370ee98fbeeb63a568eceac08ef02
“Saki <sakisandra.c@gmail.com>
2023-03-25 10:55:50 -0600

    Create passing birthday functions

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,28 @@
+function createBirthday(name, month, day){
+    let bday = {
+        name: name,
+        month: month,
+        day: day,
+    }
+    return bday;
+}
 
+function celebrateBirthday(bday){
+    return `Today is ${bday.month}/${bday.day}! Happy birthday, ${bday.name}!`
+}
 
-module.exports = {  };
+function countBirthdays(birthdays, month){
+    let bdays = 0;
+    for(let i=0; i < birthdays.length; i++){
+        if(birthdays[i].month === month){
+            bdays++;
+        }
+    } 
+    return bdays
+}
+
+module.exports = { 
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays,
+ };
