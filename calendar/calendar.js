c19fbe232f889013b33b13911e7a2990e9cf6c27
“Saki <sakisandra.c@gmail.com>
2023-03-27 15:59:10 -0600

    Create passing calendar functions

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,41 @@
+function createEvent(title, month, day) {
+  if (day <= 31 && day > 0){
+    let event = {
+      title: title,
+      month: month,
+      day: day
+    };
 
+    return event;3
+  } else {
 
-module.exports = {  };
+    return `Error: ${day} is not a valid day`
+  };
+}
+
+function createCalendar(name, events) {
+  let calendar = {
+    owner: name,
+    events: events
+  };
+
+  return calendar;
+}
+
+function reportMonthlyEvents(calendar, month) {
+  let events = [];
+  
+  for (let i=0; i < calendar.events.length; i++){
+    if (calendar.events[i].month === month){
+      events.push(calendar.events[i])
+    };
+  };
+
+  return events;
+}
+
+module.exports = {
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents,
+  };
